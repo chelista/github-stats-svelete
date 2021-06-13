8f2c9c3e05ee42dc95207a252afc0269b18f542b
Alvaro Zuniga <alvaro@usatestprep.com>
2021-06-13 00:56:42 -0400

    Implements switch with dispatcher to trigger auto refresh

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,51 @@
 <script>
 import RepoGrid from './components/RepoGrid.svelte';
 import UserGrid from './components/UserGrid.svelte';
+import {getLastMonth,getLastYear} from "./helpers/date";
+import TitleWithSwitch from './components/TitleWithSwitch.svelte';
+
+let repoRefresh = false;
+let userRefresh = false;
+const handleRepoSwitch = e => {
+	repoRefresh = e.detail.on === true;
+}
+
+const handleUserSwitch = e => {
+	userRefresh = e.detail.on === true;
+}
 </script>
 
-<main>
-	<RepoGrid />
-	<UserGrid />
+<main class="container">
+
+	<TitleWithSwitch on:switch={handleRepoSwitch}>
+		<h2>Top 5 Repositories
+			<span>with most stars since {getLastYear()}</span>
+		</h2>
+	</TitleWithSwitch>
+	<RepoGrid refresh={repoRefresh}/>
+
+
+	<TitleWithSwitch on:switch={handleUserSwitch}>
+		<h2>Top 5 Users
+			<span>(with most followers since {getLastMonth()})</span>
+		</h2>
+	</TitleWithSwitch>
+	<UserGrid refresh="{userRefresh}"/>
 </main>
 
 <style>
+	h2 {
+		font-size:2em;
+		margin-bottom: 0;
+		margin-right: 0;
+	}
+	h2 span {
+		font-size: .6em;
+		font-style: italic;
+		margin: 0;
+		font-weight: normal;
+
+	}
 </style>
 
 
---
+++ src/components/TitleWithSwitch.svelte
@@ -0,0 +1,25 @@
+<script>
+import {createEventDispatcher} from 'svelte';
+
+const dispatch = createEventDispatcher();
+let on = false;
+function handleClick() {
+    on = !on;
+    dispatch('switch', {on});
+}
+</script>
+
+<div>
+    <slot />
+    <span class="switch">
+        <label>
+            Off
+            <input type="checkbox" on:click={handleClick}>
+            <span class="lever"></span>
+            Auto Refresh
+        </label>
+    </span>
+</div>
+
+<style>
+</style>
